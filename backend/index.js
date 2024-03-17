b2345c6455e8462f493236eb4632e32b42bcc9cd
jekburri <onorbtanko@gmail.com>
2024-03-17 16:56:41 -0700

    added .env support to backend+frontend for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require('dotenv').config();
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
@@ -30,7 +31,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: `http://${process.env.EXPRESS_URL}:5173`,
     methods: ["GET", "POST"],
   },
 });
